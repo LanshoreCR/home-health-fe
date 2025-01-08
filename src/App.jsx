bcfe58f671a64c8297470cff617a09cbc5f977bd
Carlos <carlosvillalobos@lanshore.com>
2025-01-08 12:16:57 -0600

    Okta Auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import AppRouter from './AppRouter'
-// import OktaProvider from './shared/components/providers/okta-provider'
+import OktaProvider from './shared/components/providers/okta-provider'
 import Navbar from './shared/components/navbar'
 import { Route, Routes } from 'react-router-dom'
 import { LoginCallback } from '@okta/okta-react'
@@ -9,26 +9,26 @@ import QualityAuditThemeProvider from './shared/components/providers/theme-provi
 
 function App() {
 
-  // <OktaProvider>
-  {/* </OktaProvider> */ }
   return (
-    <QualityAuditThemeProvider>
-      <div className='w-full h-full'>
-        <header>
-          <Navbar />
-        </header>
-        <main className='max-w-7xl px-5 mx-auto h-full '>
-          <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <section className='h-full w-full mt-5'>
-              <Routes>
-                <Route path='/login/callback' element={<LoginCallback />} />
-                <Route path='/*' exact element={<AppRouter />} />
-              </Routes>
-            </section>
-          </LocalizationProvider>
-        </main>
-      </div>
-    </QualityAuditThemeProvider>
+    <OktaProvider>
+      <QualityAuditThemeProvider>
+        <div className='w-full h-full'>
+          <header>
+            <Navbar />
+          </header>
+          <main className='max-w-7xl px-5 mx-auto h-full '>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <section className='h-full w-full mt-5'>
+                <Routes>
+                  <Route path='/login/callback' element={<LoginCallback />} />
+                  <Route path='/*' exact element={<AppRouter />} />
+                </Routes>
+              </section>
+            </LocalizationProvider>
+          </main>
+        </div>
+      </QualityAuditThemeProvider>
+    </OktaProvider>
   )
 }
 
--- src/Root.jsx
+++ src/Root.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { Route, Routes } from 'react-router-dom'
-// import { useSession } from './shared/hooks/useSession'
+import { useSession } from './shared/hooks/useSession'
 import apiMaster from './shared/services/api/api-master'
 import { setUser } from './shared/redux/slices/user'
 import { getUserInfo } from './shared/services/api/endpoints/user-info'
@@ -12,13 +12,13 @@ import CreateToolPage from './pages/tools/create-tool/page'
 
 const RootPage = () => {
   const dispatch = useDispatch()
-  // const { token } = useSession()
-  const token = 'eyJraWQiOiJhOVQ0dHJhNE56OWZTOElvaTJTSG96bTVrRXNfd2JGajdmVkRmclgxM2dNIiwidHlwIjoiYXBwbGljYXRpb25cL29rdGEtaW50ZXJuYWwtYXQrand0IiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULnBidHd6UGk5WmFPcmZ2endOT1JROVJ5SzNXNGVnX3dpZVFZQUVzV25vMzgub2FyMXFoeHdoekt5TEtNeWoweDciLCJpc3MiOiJodHRwczovL3Jlc2NhcmUub2t0YS5jb20iLCJhdWQiOiJodHRwczovL3Jlc2NhcmUub2t0YS5jb20iLCJzdWIiOiIwNzczNDcwQHJlc2NhcmUuY29tIiwiaWF0IjoxNzM2MzU1MzkxLCJleHAiOjE3MzYzNTg5OTEsImNpZCI6IjBvYXZwc3lrNGczOXZUR242MHg3IiwidWlkIjoiMDB1MTV3YmNhZXJscG82VXIweDgiLCJzY3AiOlsib3BlbmlkIiwib2ZmbGluZV9hY2Nlc3MiXSwiYXV0aF90aW1lIjoxNzM2MzU1MzkwfQ.IBYD4QjZ4ViWRKmd73rKx3auwclKvTdCdKKRxNO1Al-f8ZAgUKbn_MdhBbdMsdQTlZi65zp_q_UyEcA6D8U1jpW7SN4H-8QuKS2ccIhnS_QWwxnWfm00BboPb9GFPe2c4zu8GnRqTgA7uQJxLXBT8upQavDrL4d6oPAUg6PoANDmQdmM41jS9NsK8bAAPEZe-gb-2DFlQzKCMXrPEc9YZB6jMWuKy9sTlA6yJeT7UVCb5TYfGz6RAYPX2E9viaICypy20gaxuYYbIFv3HqcnXcQRyT0LbgNGPzHY2hBclB7elsNtadQ_XXFneUHG4IKtyXT9U_elytsiZNyW4-WjVA'
+  const { token } = useSession()
+  // const token = 'eyJraWQiOiJhOVQ0dHJhNE56OWZTOElvaTJTSG96bTVrRXNfd2JGajdmVkRmclgxM2dNIiwidHlwIjoiYXBwbGljYXRpb25cL29rdGEtaW50ZXJuYWwtYXQrand0IiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULnBidHd6UGk5WmFPcmZ2endOT1JROVJ5SzNXNGVnX3dpZVFZQUVzV25vMzgub2FyMXFoeHdoekt5TEtNeWoweDciLCJpc3MiOiJodHRwczovL3Jlc2NhcmUub2t0YS5jb20iLCJhdWQiOiJodHRwczovL3Jlc2NhcmUub2t0YS5jb20iLCJzdWIiOiIwNzczNDcwQHJlc2NhcmUuY29tIiwiaWF0IjoxNzM2MzU1MzkxLCJleHAiOjE3MzYzNTg5OTEsImNpZCI6IjBvYXZwc3lrNGczOXZUR242MHg3IiwidWlkIjoiMDB1MTV3YmNhZXJscG82VXIweDgiLCJzY3AiOlsib3BlbmlkIiwib2ZmbGluZV9hY2Nlc3MiXSwiYXV0aF90aW1lIjoxNzM2MzU1MzkwfQ.IBYD4QjZ4ViWRKmd73rKx3auwclKvTdCdKKRxNO1Al-f8ZAgUKbn_MdhBbdMsdQTlZi65zp_q_UyEcA6D8U1jpW7SN4H-8QuKS2ccIhnS_QWwxnWfm00BboPb9GFPe2c4zu8GnRqTgA7uQJxLXBT8upQavDrL4d6oPAUg6PoANDmQdmM41jS9NsK8bAAPEZe-gb-2DFlQzKCMXrPEc9YZB6jMWuKy9sTlA6yJeT7UVCb5TYfGz6RAYPX2E9viaICypy20gaxuYYbIFv3HqcnXcQRyT0LbgNGPzHY2hBclB7elsNtadQ_XXFneUHG4IKtyXT9U_elytsiZNyW4-WjVA'
 
   useEffect(() => {
     if (token != null) {
-      // apiMaster.setToken(token.accessToken)
-      apiMaster.setToken(token)
+      apiMaster.setToken(token.accessToken)
+      // apiMaster.setToken(token)
 
       getUserInfo().then(value => {
         dispatch(setUser(value))
